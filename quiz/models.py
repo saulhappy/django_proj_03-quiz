e1ec797ff7941527c46fd04d71c2577968fc1903
saulhappy <646634@gmail.com>
2022-02-08 17:58:51 -0600

    update question model

--- quiz/models.py
+++ quiz/models.py
@@ -16,7 +16,7 @@ class Quizzes(models.Model):
         return self.title
 
 class Question(models.Model):
-    SCALE = (
+    SCALE_CHOICES = (
         (0, 'Fundamental'),
         (1, 'Beginner'),
         (2, 'Intermediate'),
@@ -24,10 +24,24 @@ class Question(models.Model):
         (4, 'Expert')
     )
 
-    TYPE = (
-        (0, 'Multiple Choice')
+    METHOD_CHOICES = (
+        (0, 'Multiple Choice'),
+        (1, 'True or False'),
     )
 
-    
+    quiz = models.ForeignKey(Quizzes, related_name='question', on_delete=models.DO_NOTHING)
+    method = models.IntegerField(choices=METHOD_CHOICES, default=0, verbose_name="Type of Question")
+    title = models.CharField(max_length=140, verbose_name="Title")
+    difficulty = models.IntegerField(choices=SCALE_CHOICES, default=0, verbose_name="Level of Difficulty")
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name="Date Created")
+    is_active = models.BooleanField(default=False, verbose_name="Active Status")
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
 
 
