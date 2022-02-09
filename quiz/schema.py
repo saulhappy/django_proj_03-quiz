e485467dcf0c4c10d3a388b9108cb2d54ad61b1a
saulhappy <646634@gmail.com>
2022-02-09 09:53:36 -0600

    add quiz schema

---
+++ quiz/schema.py
@@ -0,0 +1,25 @@
+import graphene
+from graphene_django import DjangoObjectType, DjangoListField
+from quiz.models import Quizzes, Category, Question, Answer
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ("id", "name")
+
+class QuizzesType(DjangoObjectType):
+    class Meta:
+        model = Quizzes
+        fields = ("id", "title", "category", "quiz")
+
+
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        fields = ("title", "quiz")
+
+
+class AnswerType(DjangoObjectType):
+    class Meta:
+        model = Answer
+        fields = ("question", "answer_text")
